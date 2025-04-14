b09881120e4975fe0729a04f0a4dc3415eb9ed27
Azahar <Azahar@ml.com>
2025-04-14 05:55:05 -0500

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    author="Azahar",
+    author_email="azahar@gmail.com",
+    packages=find_packages()
+)
